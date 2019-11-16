d7acdbc2fb1cd62190e090a85b2343fa4b31b4ea
Inzoid <inzoid21@gmail.com>
2019-11-17 00:33:36 +0700

    sentinel register

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin', 'JobController@index');
+
+//route signup atau register user
+Route::get('signup', 'UsersController@signup')->name('signup');
+Route::post('signup', 'UsersController@signup_store')->name('signup.store');
 
